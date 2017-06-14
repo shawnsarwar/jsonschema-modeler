ab51bf97bea416ba838aefb5fd23043e9ec848b5
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2017-06-14 18:05:07 -0400

    got recursive constraints working. Started framing out space for schema output translater

--- js/index.js
+++ js/index.js
@@ -8125,8 +8125,35 @@ JSONEditor.defaults.resolvers.unshift(function(schema) {
 
 __webpack_require__(0);
 
-// Set default options
-JSONEditor.defaults.options.theme = 'bootstrap2';
+
+function translateSchema(schema, path = ""){
+    console.log("path", path);
+    for(var key in schema) {
+        var node = schema[key]
+        console.log("node", node)
+        if (!schema.hasOwnProperty(key)){continue;}
+        if (isObject(node) || isArray(node)){
+            console.log('down into', key, node);
+            translateSchema(node, path+"/"+key);
+        }
+        else{
+            console.log("terminal", node);
+        }
+    };
+}
+
+
+function isObject(obj) {
+  return obj !== null && typeof obj === 'object';
+}
+
+function isArray(obj){
+  return obj !== null && typeof obj === 'array';
+}
+
+
+
+window.translateSchema = translateSchema;
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 require("json-editor");
 
-// Set default options
-JSONEditor.defaults.options.theme = 'bootstrap2';
+
+function translateSchema(schema, path = ""){
+    console.log("path", path);
+    for(var key in schema) {
+        var node = schema[key]
+        console.log("node", node)
+        if (!schema.hasOwnProperty(key)){continue;}
+        if (isObject(node) || isArray(node)){
+            console.log('down into', key, node);
+            translateSchema(node, path+"/"+key);
+        }
+        else{
+            console.log("terminal", node);
+        }
+    };
+}
+
+
+function isObject(obj) {
+  return obj !== null && typeof obj === 'object';
+}
+
+function isArray(obj){
+  return obj !== null && typeof obj === 'array';
+}
+
+
+
+window.translateSchema = translateSchema;
 
